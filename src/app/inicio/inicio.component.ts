5c8733b9afacdc4505ffb5d5cb17cebbff8e0ed4
DanielMendes010 <danielinacio101@gmail.com>
2021-10-12 19:51:09 -0300

    Adcionando put e delete de postagem

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,12 @@
 import { Router } from '@angular/router';
 import { environment } from './../../environments/environment.prod';
 import { Component, OnInit } from '@angular/core';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,13 +15,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+
+  usuario: Usuario = new Usuario()
+  idUsuario = environment.id
+
+  constructor(
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    private auth: AuthService
+    ) { }
 
   ngOnInit() {
     
     if(environment.token == ''){
       this.router.navigate(['/entrar'])
     }
+
+    this.auth.refreshToken();
+
+    this.getAllTema()
+    this.getAllPostagens()
   }
+
+  getAllTema(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getTemaById(this.idTema).subscribe((resp: Tema) => {
+      this.tema = resp
+    })
+  }
+
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => {
+      this.listaPostagens = resp
+    })
+  }
+
+  findByIdUsuario(){
+    this.auth.getByIdUsuario(this.idUsuario).subscribe((resp: Usuario) => {
+      this.usuario = resp
+    })
+  }
+
+  publicar(){
+    this.tema.id = this.idTema;
+    this.postagem.tema = this.tema;
+
+    this.usuario.id = this.idUsuario;
+    this.postagem.usuario = this.usuario;
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
+      this.postagem = resp
+      alert('Postagem realizada com sucesso!')
+      this.postagem = new Postagem()
+      this.getAllPostagens()
+    })
+
+  }
+
 
 }
